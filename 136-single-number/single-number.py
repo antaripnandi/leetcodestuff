23b3a5ff1965d7a12b7701ba860b5acf4a23c08f
antaripbozo <72333919+antaripnandi@users.noreply.github.com>
2026-09-25 21:57:26 +0530

    Time: 4759 ms (7.11%) | Memory: 14 MB (30.88%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        for i in range(len(nums)):
+            if nums.count(nums[i])==1:
+                return nums[i]
+        return []
+        
